4755fcf910ee64f2e130fe5fafb004ada2652547
mateus.chagas <mateus.chagas@avancoinfo.com.br>
2025-12-02 21:05:41 -0300

    :sparkles:feat: integra supabase ao projeto e adiciona servico para injecao

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  SUPABASE_URL: 'https://xkgpgtjddlsomcsemvbb.supabase.co',
+  SUPABASE_KEY:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhrZ3BndGpkZGxzb21jc2VtdmJiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ1MTY5NzYsImV4cCI6MjA4MDA5Mjk3Nn0.VVdTBFNFcpWiNqeu5BQ6srO587axRVDD_2rDOHvh2Dc',
+};
