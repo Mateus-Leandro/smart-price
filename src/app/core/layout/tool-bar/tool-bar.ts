5366d32324d8035b630a1627b22515811c18a6a9
Mateus Chagas <mateusleandro2205@gmail.com>
2025-12-06 14:31:53 -0300

    :sparkles:feat: adiciona botão de sair ao toolbar

--- src/app/core/layout/tool-bar/tool-bar.ts
+++ src/app/core/layout/tool-bar/tool-bar.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { AuthService } from 'src/app/features/auth/services/auth.service';
+import { Button } from "src/app/shared/components/button/button";
 
 @Component({
   selector: 'app-tool-bar',
-  imports: [MatToolbarModule],
+  imports: [MatToolbarModule, MatIconModule, Button],
   templateUrl: './tool-bar.html',
   styleUrl: '../../../global/styles/_toolbar.scss',
 })
-export class ToolBar {}
+export class ToolBar {
+
+  constructor(private authService: AuthService){
+
+  }
+
+  logout() {
+    this.authService.logout();
+  }
+}
