12a6219956cb6e685c5dc0a64bb50f8f7a5dd8a0
mateus.chagas <mateus.chagas@avancoinfo.com.br>
2025-12-08 16:54:56 -0300

    :lipstick:feat: atualiza estilos e logica do botão para suporte a cores dinamicas

--- src/app/shared/components/button/button.ts
+++ src/app/shared/components/button/button.ts
@@ -9,19 +9,18 @@ import { Spinner } from '../spinner/spinner';
   selector: 'app-button',
   imports: [MatButtonModule, MatIcon, CommonModule, MatProgressSpinnerModule, Spinner],
   templateUrl: './button.html',
-  styleUrl: '../../../global/styles/_buttons.scss',
 })
 export class Button {
-  @Input() text: string = 'Botão';
-  @Input() icon: string = '';
-  @Input() bgColor: string = '#000';
-  @Input() textColor: string = 'white';
+  @Input() text?: string;
+  @Input() icon?: string;
+  @Input() bgColor?: string = 'var(--primary)';
+  @Input() textColor: string = 'var(--font-primary)';
   @Input() height: string = '40px';
   @Input() width: string = 'auto';
   @Input() fontSize: string = '16px';
   @Input() padding: string = '0 24px';
   @Input() type: 'button' | 'submit' = 'button';
-  @Input() disabled: boolean = false;
+  @Input() disabled?: boolean = false;
   @Input() loading: boolean = false;
   @Input() spinnerDiameter = 70;
 }
