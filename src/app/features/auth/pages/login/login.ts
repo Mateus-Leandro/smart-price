469a348605ea5fdd7e4105e3192204fc6fb0cf7f
mateus.chagas <mateus.chagas@avancoinfo.com.br>
2025-12-16 07:56:59 -0300

    :sparkles:feat: inclui mensagem de retorno referente a credenciais invalidas

--- src/app/features/auth/pages/login/login.ts
+++ src/app/features/auth/pages/login/login.ts
@@ -1,44 +1,75 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatInputModule } from '@angular/material/input';
-import { FormsModule, NgForm } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
 import { Button } from 'src/app/shared/components/button/button';
 import { CommonModule } from '@angular/common';
-import { ChangeDetectorRef } from '@angular/core';
+import { LoginErrorStateMatcher } from 'src/app/shared/matchers/login-error-state-matcher';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FlexLayoutModule, MatInputModule, FormsModule, Button, CommonModule],
+  imports: [
+    FlexLayoutModule,
+    MatInputModule,
+    FormsModule,
+    Button,
+    CommonModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
 export class Login {
-  email = '';
-  password = '';
   loading = false;
+  loginFormGroup: FormGroup;
+  matcher = new LoginErrorStateMatcher();
 
   constructor(
     private authService: AuthService,
     private cdr: ChangeDetectorRef,
-  ) {}
+    private fb: FormBuilder,
+  ) {
+    this.loginFormGroup = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    });
+  }
 
-  async login(form: NgForm) {
+  async onSubmit() {
+    const passwordCtrl = this.loginFormGroup.get('password');
+    passwordCtrl?.setErrors(null);
+
+    this.loginFormGroup.markAllAsTouched();
+    if (this.loginFormGroup.invalid) return;
+
     this.loading = true;
 
-    if (form.invalid) {
-      form.control.markAllAsTouched();
-      this.loading = false;
-      return;
-    }
-
     try {
-      await this.authService.login(this.email, this.password);
+      await this.authService.login(
+        this.loginFormGroup.value.email,
+        this.loginFormGroup.value.password,
+      );
     } catch (error) {
+      this.loginFormGroup.setErrors({ invalidCredential: true });
+      this.loginFormGroup.markAllAsTouched();
     } finally {
       this.loading = false;
       this.cdr.detectChanges();
     }
+  }
+
+  get email() {
+    return this.loginFormGroup.get('email')!;
+  }
+  get password() {
+    return this.loginFormGroup.get('password')!;
   }
 }
