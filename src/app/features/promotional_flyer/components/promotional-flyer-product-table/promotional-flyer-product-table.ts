153d6ecb723669648461af0e403a98ad2585bf5f
Mateus Chagas <mateusleandro2205@gmail.com>
2025-12-22 00:33:57 -0300

    :sparkles:feat: input para pesquisa de item pela descricao

--- src/app/features/promotional_flyer/components/promotional-flyer-product-table/promotional-flyer-product-table.ts
+++ src/app/features/promotional_flyer/components/promotional-flyer-product-table/promotional-flyer-product-table.ts
@@ -8,16 +8,35 @@ import { Spinner } from 'src/app/shared/components/spinner/spinner';
 import { IconButton } from 'src/app/shared/components/icon-button/icon-button';
 import { MatPaginator, MatPaginatorModule, PageEvent } from '@angular/material/paginator';
 import { IDefaultPaginatorDataSource } from 'src/app/shared/interfaces/query-interface';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatIconModule } from '@angular/material/icon';
+import { debounceTime } from 'rxjs/internal/operators/debounceTime';
+import { distinctUntilChanged } from 'rxjs/internal/operators/distinctUntilChanged';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-promotional-flyer-product-table',
-  imports: [Spinner, IconButton, MatTableModule, MatSortModule, MatPaginator, MatPaginatorModule],
+  imports: [
+    Spinner,
+    IconButton,
+    MatTableModule,
+    MatSortModule,
+    MatPaginator,
+    MatPaginatorModule,
+    MatInputModule,
+    FormsModule,
+    FlexLayoutModule,
+    MatIconModule,
+  ],
   templateUrl: './promotional-flyer-product-table.html',
   styleUrl: './promotional-flyer-product-table.scss',
 })
 export class PromotionalFlyerProductTable {
   @ViewChild(MatSort) sort!: MatSort;
 
+  searchTerm = '';
   loading = false;
   flyerId: number = 0;
   sortEvent!: Sort;
@@ -37,9 +56,16 @@ export class PromotionalFlyerProductTable {
     private route: ActivatedRoute,
   ) {}
 
-  ngOnInit(): void {
-    this.flyerId = Number(this?.route?.snapshot?.paramMap?.get('id'));
-    this.loadProductsFromPromotionalFlyer(this.flyerId, this.paginatorDataSource);
+  private search$ = new Subject<string>();
+
+  ngOnInit() {
+    this.flyerId = Number(this.route.snapshot.paramMap.get('id'));
+    this.reload();
+    this.search$.pipe(debounceTime(300), distinctUntilChanged()).subscribe((value) => {
+      this.searchTerm = value;
+      this.paginatorDataSource.pageIndex = 0;
+      this.reload();
+    });
   }
 
   ngAfterViewInit() {
@@ -49,6 +75,7 @@ export class PromotionalFlyerProductTable {
   async loadProductsFromPromotionalFlyer(
     flyerId: number,
     paginatorDataSource: IDefaultPaginatorDataSource<IPromotionalFlyerProducts>,
+    search?: string,
   ) {
     this.loading = true;
 
@@ -56,6 +83,7 @@ export class PromotionalFlyerProductTable {
       this.paginatorDataSource.records = await this.promotionalFlyerService.loadProducts(
         flyerId,
         paginatorDataSource,
+        search,
       );
       this.dataSource.data = this.paginatorDataSource.records.data;
     } catch (err) {
@@ -69,6 +97,15 @@ export class PromotionalFlyerProductTable {
   onPageChange(event: PageEvent) {
     this.paginatorDataSource.pageSize = event.pageSize;
     this.paginatorDataSource.pageIndex = event.pageIndex;
-    this.loadProductsFromPromotionalFlyer(this.flyerId, this.paginatorDataSource);
+    this.reload();
+  }
+
+  onSearch(event: Event) {
+    const value = (event.target as HTMLInputElement).value;
+    this.search$.next(value);
+  }
+
+  private reload() {
+    this.loadProductsFromPromotionalFlyer(this.flyerId, this.paginatorDataSource, this.searchTerm);
   }
 }
