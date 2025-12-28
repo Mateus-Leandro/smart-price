33360e6f495b63895e20ce40dd4da89699054e98
Mateus Chagas <mateusleandro2205@gmail.com>
2025-12-27 23:27:54 -0300

    :sparkles:feat: cria botao para retornar para a lista de encartes

--- src/app/features/promotional_flyer/pages/promotional-flyer-products/promotional-flyer-products.ts
+++ src/app/features/promotional_flyer/pages/promotional-flyer-products/promotional-flyer-products.ts
@@ -1,23 +1,36 @@
-import { ChangeDetectorRef, Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { PromotionalFlyerService } from '../../services/promotional-flyer.service';
-import { MatTableDataSource } from '@angular/material/table';
+import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { MatCard, MatCardHeader, MatCardTitle, MatCardSubtitle } from '@angular/material/card';
 import { PromotionalFlyerProductTable } from '../../components/promotional-flyer-product-table/promotional-flyer-product-table';
+import { IconButton } from 'src/app/shared/components/icon-button/icon-button';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @Component({
   selector: 'app-promotional-flyer-products',
-  imports: [MatCard, MatCardHeader, MatCardTitle, MatCardSubtitle, PromotionalFlyerProductTable],
+  imports: [
+    MatCard,
+    MatCardHeader,
+    MatCardTitle,
+    MatCardSubtitle,
+    PromotionalFlyerProductTable,
+    IconButton,
+    FlexLayoutModule,
+  ],
   templateUrl: './promotional-flyer-products.html',
   styleUrl: './promotional-flyer-products.scss',
 })
 export class PromotionalFlyerProducts {
-  loading = false;
-  dataSource = new MatTableDataSource<PromotionalFlyerProducts>([]);
   id: number = 0;
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+  ) {}
 
   ngOnInit() {
     this.id = Number(this?.route?.snapshot?.paramMap?.get('id'));
+  }
+
+  goBack() {
+    this.router.navigate(['/promotional_flyer']);
   }
 }
