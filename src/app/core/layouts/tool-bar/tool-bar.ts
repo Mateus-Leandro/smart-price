4eaecbad40eefb63c1a90352329677ae2697f6fa
Mateus Chagas <mateusleandro2205@gmail.com>
2026-01-03 11:11:15 -0300

    :lipstick:feat: exibir usuario na toolbar

--- src/app/core/layouts/tool-bar/tool-bar.ts
+++ src/app/core/layouts/tool-bar/tool-bar.ts
@@ -1,6 +1,8 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { ChangeDetectorRef, Component, EventEmitter, Output } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { User } from '@supabase/supabase-js';
+import { AuthService } from 'src/app/features/auth/services/auth.service';
 import { IconButton } from 'src/app/shared/components/icon-button/icon-button';
 
 @Component({
@@ -10,6 +12,24 @@ import { IconButton } from 'src/app/shared/components/icon-button/icon-button';
   styleUrl: '../../../global/styles/_toolbar.scss',
 })
 export class ToolBar {
-  constructor() {}
+  user: User | null = null;
+  constructor(
+    private authService: AuthService,
+    private cdr: ChangeDetectorRef,
+  ) {}
   @Output() menuClick = new EventEmitter<void>();
+
+  async ngOnInit() {
+    await this.loadUser();
+  }
+
+  async loadUser() {
+    try {
+      this.user = await this.authService.getUser();
+    } catch (error) {
+      console.error('Usuário não autenticado na Toolbar', error);
+    } finally {
+      this.cdr.detectChanges();
+    }
+  }
 }
