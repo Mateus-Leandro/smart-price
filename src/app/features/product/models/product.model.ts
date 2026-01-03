414b089709171508a12c68d26a5a76ff30f19464
Mateus Chagas <mateusleandro2205@gmail.com>
2026-01-03 15:50:14 -0300

    :sparkles:feat: cria models service e repository para a entidade produto

---
+++ src/app/features/product/models/product.model.ts
@@ -0,0 +1,14 @@
+export interface IProduct {
+  id: number;
+  companyId: number;
+  name: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface IProductView {
+  id: number;
+  name: string;
+  createdAt: string;
+  updatedAt: string;
+}
