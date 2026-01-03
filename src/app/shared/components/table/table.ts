bbded81cfcb99f83c69dba39b011c750ae246f5b
Mateus Chagas <mateusleandro2205@gmail.com>
2026-01-03 15:50:48 -0300

    :sparkles:feat: cria component reutilizavel table

--- src/app/shared/components/table/table.ts
+++ src/app/shared/components/table/table.ts
@@ -1,9 +1,42 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output, TemplateRef } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
+import { MatTableModule } from '@angular/material/table';
+import { TableColumn } from 'src/app/core/models/table-app.model';
 
 @Component({
   selector: 'app-table',
-  imports: [],
+  imports: [MatButtonModule, MatPaginatorModule, MatTableModule, CommonModule, MatIconModule],
   templateUrl: './table.html',
-  styleUrl: '../../../global/styles/_tables.scss',
+  styleUrl: './table.scss',
 })
-export class Table {}
+export class Table {
+  @Input() data: any[] = [];
+  @Input() totalCount: number = 0;
+  @Input() pageSize: number = 10;
+  @Input() pageIndex: number = 0;
+  @Input() columns: TableColumn[] = [];
+
+  @Input() expandedDetailTemplate!: TemplateRef<any>;
+
+  @Output() pageChange = new EventEmitter<PageEvent>();
+  @Output() rowToggle = new EventEmitter<any>();
+
+  expandedElement: any | null = null;
+
+  get displayedColumns(): string[] {
+    return ['expand', ...this.columns.map((c) => c.key)];
+  }
+
+  toggleRow(element: any) {
+    this.expandedElement = this.expandedElement === element ? null : element;
+    this.rowToggle.emit(this.expandedElement);
+  }
+
+  onPageChange(event: PageEvent) {
+    this.expandedElement = null;
+    this.pageChange.emit(event);
+  }
+}
