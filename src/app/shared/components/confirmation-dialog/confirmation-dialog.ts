b21c7bf3cb47d92b6226b0c779e940b9c1ee613b
Mateus Chagas <mateusleandro2205@gmail.com>
2026-01-30 18:33:12 -0300

    :sparkles:feat: cria dialog de confirmacao

---
+++ src/app/shared/components/confirmation-dialog/confirmation-dialog.ts
@@ -0,0 +1,26 @@
+import { Component, Inject } from '@angular/core';
+import { MatDialogModule, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { Button } from '../button/button';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+export interface DialogData {
+  titleText?: string;
+  messageText: string;
+  cancelText?: string;
+  confirmationText?: string;
+}
+
+@Component({
+  selector: 'app-confirmation-dialog',
+  standalone: true,
+  imports: [MatDialogModule, MatButtonModule, Button, FlexLayoutModule],
+  templateUrl: './confirmation-dialog.html',
+})
+export class ConfirmationDialog {
+  constructor(@Inject(MAT_DIALOG_DATA) public data: DialogData) {
+    this.data.titleText ??= 'Atenção';
+    this.data.cancelText ??= 'Cancelar';
+    this.data.confirmationText ??= 'Confirmar';
+  }
+}
